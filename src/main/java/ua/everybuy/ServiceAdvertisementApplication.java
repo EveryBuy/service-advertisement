f6da8d364fd698ae4a36032a97c547b9271ec5c0
smagles <lesialist@icloud.com>
2024-04-15 17:22:39 +0200

    Initial Docker, configurations, dependencies settings

---
+++ src/main/java/ua/everybuy/ServiceAdvertisementApplication.java
@@ -0,0 +1,13 @@
+package ua.everybuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceAdvertisementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceAdvertisementApplication.class, args);
+	}
+
+}
