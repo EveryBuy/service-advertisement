360e2e42feddbcb1b333ed37f0b44fc20ca80ecd
smagles <lesialist@icloud.com>
2024-05-12 19:09:30 +0200

    Create SubCategoryController

--- src/main/java/ua/everybuy/routing/controller/SubCategoryController.java
+++ src/main/java/ua/everybuy/routing/controller/SubCategoryController.java
@@ -3,38 +3,29 @@ package ua.everybuy.routing.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-import ua.everybuy.buisnesslogic.service.CategoryService;
-import ua.everybuy.database.entity.Category;
+import ua.everybuy.buisnesslogic.service.SubCategoryService;
+import ua.everybuy.database.entity.SubCategory;
 import ua.everybuy.routing.dto.StatusResponse;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/ad/category")
+@Controller
 @RequiredArgsConstructor
-public class CategoryController {
-
-    private final CategoryService categoryService;
+@RequestMapping("/ad/subcategory")
+public class SubCategoryController {
+    private final SubCategoryService subCategoryService;
 
     @GetMapping()
     @ResponseStatus(HttpStatus.OK)
-    public List<Category> getAllCategory() {
-        return categoryService.getAllCategories();
-    }
-
-    @GetMapping("/ukr")
-    @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<StatusResponse> getAllUkrNames() {
-        List<String> ukrCategories = categoryService.getAllUkrCategories();
+    public ResponseEntity<StatusResponse> getAllSubcategory() {
+        List<SubCategory> subCategories = subCategoryService.getAllSubCategories();
         return ResponseEntity.ok(StatusResponse.builder()
                 .status(HttpStatus.OK.value())
-                .data(ukrCategories)
+                .data(subCategories)
                 .build());
     }
-
-
 }
