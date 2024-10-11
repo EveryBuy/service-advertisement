0176ca63f5818e8503adea9535739e1e68b01a1e
smagles <lesialist@icloud.com>
2024-10-11 19:43:01 +0200

    Add support for 'Other' delivery option

---
+++ src/main/java/ua/everybuy/buisnesslogic/strategy/delivery/OtherDeliveryMethod.java
@@ -0,0 +1,8 @@
+package ua.everybuy.buisnesslogic.strategy.delivery;
+
+public class OtherDeliveryMethod implements DeliveryMethod {
+    @Override
+    public String getName() {
+        return "OTHER";
+    }
+}
