9cf8f28990717b68f6412457dd5ebeedb9ab6e9b
smagles <lesialist@icloud.com>
2024-10-18 14:18:10 +0200

    changed method getActiveAdvertisements()

--- src/main/java/ua/everybuy/database/repository/AdvertisementRepository.java
+++ src/main/java/ua/everybuy/database/repository/AdvertisementRepository.java
@@ -1,7 +1,5 @@
 package ua.everybuy.database.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ua.everybuy.database.entity.Advertisement;
@@ -12,5 +10,5 @@ import java.util.Optional;
 public interface AdvertisementRepository extends JpaRepository<Advertisement, Long> {
     Optional <Advertisement> findByIdAndUserId(Long id, Long userId);
     List<Advertisement> findByUserId(Long userId);
-    Page<Advertisement> findByIsEnabledTrueOrderByCreationDateDesc(Pageable pageable);
+    List<Advertisement> findByIsEnabledTrueOrderByCreationDateDesc();
 }
