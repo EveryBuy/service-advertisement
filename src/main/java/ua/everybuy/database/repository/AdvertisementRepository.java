f1402f5eb1508903756c4664b4d1c5aaef4bee34
smagles <lesialist@icloud.com>
2025-01-05 11:15:38 +0100

    created join fetch request

--- src/main/java/ua/everybuy/database/repository/AdvertisementRepository.java
+++ src/main/java/ua/everybuy/database/repository/AdvertisementRepository.java
@@ -1,7 +1,9 @@
 package ua.everybuy.database.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ua.everybuy.database.entity.Advertisement;
 import java.util.List;
@@ -12,8 +14,12 @@ public interface AdvertisementRepository extends JpaRepository<Advertisement, Lo
         JpaSpecificationExecutor<Advertisement> {
     Optional<Advertisement> findByIdAndUserId(Long id, Long userId);
 
+    @Query("SELECT a FROM Advertisement a " +
+            "JOIN FETCH a.advertisementDeliveries " +
+            "WHERE a.userId = :userId")
     List<Advertisement> findByUserId(Long userId);
 
+    @EntityGraph(attributePaths = {"city", "lowLevelSubcategory", "topLevelSubcategory", "advertisementDeliveries"})
     List<Advertisement> findByIsEnabledTrueOrderByCreationDateDesc();
 
 }
