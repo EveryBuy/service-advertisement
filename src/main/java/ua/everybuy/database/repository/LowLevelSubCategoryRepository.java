a7ca6f859f4fb8c349b0431d7044f0c13ab12a39
smagles <lesialist@icloud.com>
2025-01-16 18:59:50 +0100

    added annotation repository

--- src/main/java/ua/everybuy/database/repository/LowLevelSubCategoryRepository.java
+++ src/main/java/ua/everybuy/database/repository/LowLevelSubCategoryRepository.java
@@ -1,9 +1,12 @@
 package ua.everybuy.database.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ua.everybuy.database.entity.LowLevelSubCategory;
+
 import java.util.List;
 
+@Repository
 public interface LowLevelSubCategoryRepository extends JpaRepository<LowLevelSubCategory, Long> {
     List<LowLevelSubCategory> findByTopLevelSubCategoryId(Long topLevelSubCategoryId);
 }
