c17d718a7cd52c9502e9391f8ddcda927bebf121
smagles <lesialist@icloud.com>
2025-04-22 17:28:37 +0200

    Return default user if user service is unavailable

--- src/main/java/ua/everybuy/buisnesslogic/service/integration/UserProfileService.java
+++ src/main/java/ua/everybuy/buisnesslogic/service/integration/UserProfileService.java
@@ -9,16 +9,27 @@ import ua.everybuy.routing.dto.response.UserShortInfoResponse;
 @Service
 @RequiredArgsConstructor
 public class UserProfileService {
-    private final ExchangeService exchangeService;
     private static final String SHORT_INFO_ENDPOINT = "/short-info?userId=";
-
     @Value("${user.service.url}")
     private String userServiceUrl;
+    @Value("${user.default.avatar.url}")
+    private String defaultAvatarUrl;
+    private final ExchangeService exchangeService;
 
     public ShortUserInfoDto getShortUserInfo(Long userId) {
-        String shortUserInfoUrl = userServiceUrl + SHORT_INFO_ENDPOINT + userId;
-        UserShortInfoResponse response = exchangeService
-                .exchangeGetRequest(shortUserInfoUrl, UserShortInfoResponse.class).getBody();
-        return response != null ? response.getData() : null;
+        try {
+            String shortUserInfoUrl = userServiceUrl + SHORT_INFO_ENDPOINT + userId;
+            UserShortInfoResponse response = exchangeService
+                    .exchangeGetRequest(shortUserInfoUrl, UserShortInfoResponse.class).getBody();
+
+            return response != null ? response.getData() : createDefaultShortUserInfo(userId);
+        } catch (Exception e) {
+            return createDefaultShortUserInfo(userId);
+        }
+    }
+
+    private ShortUserInfoDto createDefaultShortUserInfo(Long userId) {
+        return new ShortUserInfoDto(userId, "Unknown User",
+                defaultAvatarUrl);
     }
 }
