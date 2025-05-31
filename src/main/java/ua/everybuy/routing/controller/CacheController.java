e9672e0e2b76fa8c7d2838560610e034748cd8ef
smagles <lesialist@icloud.com>
2025-05-31 14:23:18 +0200

    fix: circular reference and lazy init issue in caching

---
+++ src/main/java/ua/everybuy/routing/controller/CacheController.java
@@ -0,0 +1,27 @@
+package ua.everybuy.routing.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cache")
+public class CacheController {
+
+    @Autowired
+    private CacheManager cacheManager;
+
+    @DeleteMapping("/evictAll")
+    public ResponseEntity<String> evictAllCaches() {
+        cacheManager.getCacheNames().forEach(name -> {
+            var cache = cacheManager.getCache(name);
+            if (cache != null) {
+                cache.clear();
+            }
+        });
+        return ResponseEntity.ok("All caches cleared");
+    }
+}
